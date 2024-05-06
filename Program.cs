664738bd814e76a781cc34cc3d402004ee7c8df0
Aadi <aadityakalwani@gmail.com>
2024-05-06 12:08:26 +0100

    initial code with menu system, getValidInt and subroutines to perform Dijkstra, Prims and Kruskals

--- Program.cs
+++ Program.cs
@@ -2,8 +2,61 @@
 
 class Program
 {
-    static void Main()
+
+    public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int[,] adjacencyMatrix = new int[3, 3];
+        // 3x3 matrix for now, can be changed later
+        // take an input for dimensions of the matrix / number of nodes etc. and then set up a 2d array as required later
+        
+        
+        Console.WriteLine("Enter your choice:\n1 - apply Dijkstra\n2 - apply Prim's\n3 - apply Kruskal's\n");
+        int userMenuOption = GetValidInt(1, 9999999);
+
+        switch (userMenuOption) 
+        {
+            case 1:
+                applyDijkstra(adjacencyMatrix);
+            
+                break;
+            case 2:
+                applyPrims(adjacencyMatrix);
+            
+                break;
+            case 3:
+                applyKruskal(adjacencyMatrix);
+            
+                break;
+        }
+    }
+
+    public static void applyDijkstra(int[,] adjacencyMatrix)
+    {
+        Console.WriteLine("We are applying Dijkstra; let us cook");
+    }
+
+    public static void applyPrims(int[,] adjacencyMatrix)
+    {
+        Console.WriteLine("doing Prim's");
+    }
+
+    public static void applyKruskal(int[,] adjacencyMatrix)
+    {
+        Console.WriteLine("doing Kruskal's");
+    }
+
+    public static int GetValidInt(int min, int max)
+    {
+        if (int.TryParse(Console.ReadLine(), out int validInt))
+        {
+            if (validInt >= min && validInt <= max)
+            {
+                return validInt;
+            }
+            Console.WriteLine($"No bro enter a number between {min} and {max}");
+            return GetValidInt(min, max);
+        }
+        Console.WriteLine("Please enter a valid integer");
+        return GetValidInt(min, max);
     }
 }
--- obj/Debug/net8.0/NetworkProgram.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetworkProgram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetworkProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac06f294b25edb0901a901c8f7005364107aac1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetworkProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetworkProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
